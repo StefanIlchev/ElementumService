700bb5b9e92d0a38ad7e632575453cc30959b9af
StefanIlchev <48853532+StefanIlchev@users.noreply.github.com>
2023-07-11 19:08:04 +0300

    Update elementum.cpp

--- src/main/cpp/elementum.cpp
+++ src/main/cpp/elementum.cpp
@@ -1,16 +1,34 @@
 #include "elementum.h"
 
-#include <string>
+#include <cstring>
+
+class Args {
+
+	char *args;
+
+public:
+	Args(int argc, char *argv[]) {
+		auto len = 1U;
+		for (auto i = 1; i < argc; ++i) {
+			++len;
+			len += strlen(argv[i]);
+		}
+		args = new char[len]{};
+		for (auto i = 1; i < argc; ++i) {
+			strcat(args, " ");
+			strcat(args, argv[i]);
+		}
+	}
+
+	~Args() {
+		delete[] args;
+	}
+
+	char *get() {
+		return args;
+	}
+};
 
 int main(int argc, char *argv[]) {
-	std::string str;
-	for (auto i = 1; i < argc; ++i) {
-		str += ' ';
-		str += argv[i];
-	}
-	auto args = new char[str.length() + 1];
-	strcpy(args, str.c_str());
-	auto result = static_cast<int>(startWithArgs(args));
-	delete[] args;
-	return result;
+	return static_cast<int>(startWithArgs(Args(argc, argv).get()));
 }
