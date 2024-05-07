6f7226225a63ea76357ef90005d6ade33982da83
StefanIlchev <48853532+StefanIlchev@users.noreply.github.com>
2024-05-08 02:05:57 +0300

    Update Kotlin to 1.9.24

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
 		id("com.android.library") version "8.4.0"
 
 		// https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
-		kotlin("android") version "1.9.23"
+		kotlin("android") version "1.9.24"
 
 		// https://plugins.gradle.org/plugin/com.github.breadmoirai.github-release
 		id("com.github.breadmoirai.github-release") version "2.5.2"
