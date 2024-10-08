46c51e170e039b7dc9ebea6c46881fa9c45c412d
StefanIlchev <48853532+StefanIlchev@users.noreply.github.com>
2024-10-08 22:38:30 +0300

    ElementumService 0.1.1060001
    binaryWrapper 1.0.25
    Lt2httpService 0.0.160042

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,7 +30,7 @@ dependencyResolutionManagement {
 
 			version("binaryWrapper.version", "1.0.25")
 
-			version("elementum.versionCode", "91")
+			version("elementum.versionCode", "92")
 
 			version("lt2http.versionCode", "42")
 
