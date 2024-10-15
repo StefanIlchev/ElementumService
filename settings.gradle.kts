37b78fb9bf0268cbbc709c40a2174c11e79df8bb
StefanIlchev <48853532+StefanIlchev@users.noreply.github.com>
2024-10-16 01:17:47 +0300

    Update AGP to 8.7.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,8 +11,8 @@ pluginManagement {
 	plugins {
 
 		// https://mvnrepository.com/artifact/com.android.tools.build/gradle
-		id("com.android.application") version "8.7.0"
-		id("com.android.library") version "8.7.0"
+		id("com.android.application") version "8.7.1"
+		id("com.android.library") version "8.7.1"
 
 		// https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
 		kotlin("android") version "2.0.21"
