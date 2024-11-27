581cff99316fe9ebc343f2f7b5204550db2950e6
StefanIlchev <48853532+StefanIlchev@users.noreply.github.com>
2024-11-27 20:55:55 +0200

    Update Kotlin to 2.1.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
 		id("com.android.library") version "8.7.2"
 
 		// https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
-		kotlin("android") version "2.0.21"
+		kotlin("android") version "2.1.0"
 
 		// https://plugins.gradle.org/plugin/com.github.breadmoirai.github-release
 		id("com.github.breadmoirai.github-release") version "2.5.2"
