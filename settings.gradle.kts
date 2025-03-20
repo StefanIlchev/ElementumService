6dcfa6fa1eee6d23c9aa731ecb6067c2a616269a
StefanIlchev <48853532+StefanIlchev@users.noreply.github.com>
2025-03-20 20:48:43 +0200

    Update Kotlin to 2.1.20

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
 		id("com.android.library") version "8.9.0"
 
 		// https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
-		kotlin("android") version "2.1.10"
+		kotlin("android") version "2.1.20"
 
 		// https://plugins.gradle.org/plugin/com.github.breadmoirai.github-release
 		id("com.github.breadmoirai.github-release") version "2.5.2"
