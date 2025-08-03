ea60930be7439fc8ce90059352386274bb62e4a1
StefanIlchev <48853532+StefanIlchev@users.noreply.github.com>
2025-08-03 17:40:53 +0300

    Update dependency versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ pluginManagement {
 		// https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
 		kotlin("android") version "2.2.0"
 
-		// https://plugins.gradle.org/plugin/com.github.breadmoirai.github-release
+		// https://mvnrepository.com/artifact/com.github.breadmoirai.github-release/com.github.breadmoirai.github-release.gradle.plugin
 		id("com.github.breadmoirai.github-release") version "2.5.2"
 	}
 }
@@ -54,10 +54,10 @@ dependencyResolutionManagement {
 			version("cpp", "20")
 
 			// https://mvnrepository.com/artifact/androidx.test/runner
-			library("androidTest.runner", "androidx.test:runner:1.6.2")
+			library("androidTest.runner", "androidx.test:runner:1.7.0")
 
 			// https://mvnrepository.com/artifact/androidx.test.ext/junit-ktx
-			library("androidTest.junit", "androidx.test.ext:junit-ktx:1.2.1")
+			library("androidTest.junit", "androidx.test.ext:junit-ktx:1.3.0")
 
 			// https://mvnrepository.com/artifact/junit/junit
 			library("test.junit", "junit:junit:4.13.2")
